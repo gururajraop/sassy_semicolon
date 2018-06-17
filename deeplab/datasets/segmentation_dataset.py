f0d37551bd9e9fe9605a9822ff26e70f5dd216a8
TijmenvanDijk <31856921+TijmenvanDijk@users.noreply.github.com>
2018-06-17 16:34:27 +0200

    To build tfrecord (not tested, adapted from voc 2012)

--- deeplab/datasets/segmentation_dataset.py
+++ deeplab/datasets/segmentation_dataset.py
@@ -107,11 +107,21 @@ _ADE20K_INFORMATION = DatasetDescriptor(
     ignore_label=0,
 )
 
+_MSCOCO_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 20210,  # num of samples in images/training
+        'val': 2000,  # num of samples in images/validation
+    },
+    num_classes=2,
+)
+
+
 
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'mscoco': _MSCOCO_INFORMATION,
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
